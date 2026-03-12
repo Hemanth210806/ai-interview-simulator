553911c486e8405d27e78fec588a7e677cf3d26e
Hemanth <hemanthkm2006@gmail.com>
2026-03-12 20:49:13 +0530

    fix env

--- src/config/db.js
+++ src/config/db.js
@@ -1,28 +1,22 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config();
 
 const pool = mysql.createPool({
-
-  host: process.env.DB_HOST || "mysql.railway.internal",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "railway",
-  port: process.env.DB_PORT || 3306,
-
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
-
 });
 
-// Test DB connection
 async function testConnection() {
   try {
     const connection = await pool.getConnection();
-    console.log("✅ MySQL Connected Successfully");
+    console.log("MySQL connected");
     connection.release();
-  } catch (err) {
-    console.error("❌ Database Connection Failed:", err.message);
+  } catch (error) {
+    console.error("Database connection error:", error);
   }
 }
 
